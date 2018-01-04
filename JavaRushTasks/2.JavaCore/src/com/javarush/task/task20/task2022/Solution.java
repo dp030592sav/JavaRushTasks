29a31786e79e60563eafd5cb97f0e21f99b9241f
dp030592sav <7262371@gmail.com>
2018-01-04 11:11:11 +0200

    Переопределение сериализации в потоке javarush
    task20.task2022

---
+++ JavaRushTasks/2.JavaCore/src/com/javarush/task/task20/task2022/Solution.java
@@ -0,0 +1,73 @@
+package com.javarush.task.task20.task2022;
+
+import java.io.*;
+
+/* 
+Переопределение сериализации в потоке
+Сериализация/десериализация Solution не работает.
+Исправь ошибки не меняя сигнатуры методов и класса.
+Метод main не участвует в тестировании.
+
+Написать код проверки самостоятельно в методе main:
+1) создать экземпляр класса Solution
+2) записать в него данные - writeObject
+3) сериализовать класс Solution - writeObject(ObjectOutputStream out)
+4) десериализовать, получаем новый объект
+5) записать в новый объект данные - writeObject
+6) проверить, что в файле есть данные из п.2 и п.5
+
+
+Требования:
+1. Поле stream должно быть объявлено с модификатором transient.
+2. В методе writeObject(ObjectOutputStream out) не должен быть вызван метод close у потока полученного в качестве параметра.
+3. В методе readObject(ObjectInputStream in) не должен быть вызван метод close у потока полученного в качестве параметра.
+4. В методе readObject(ObjectInputStream in) поле stream должно быть инициализировано новым объектом типа FileOutputStream с параметрами(fileName, true).
+5. В конструкторе класса Solution поле stream должно быть инициализировано новым объектом типа FileOutputStream с параметром(fileName).
+*/
+public class Solution implements Serializable, AutoCloseable {
+    private String fileName;
+    transient private FileOutputStream stream;
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
+        this.stream = new FileOutputStream(fileName);
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws Exception {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws Exception {
+        in.defaultReadObject();
+        stream = new FileOutputStream(fileName ,true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution("C:\\Users\\user\\Desktop\\qwe1.txt");
+        solution.writeObject("data");
+        solution.close();
+
+        FileOutputStream fileOutputStream = new FileOutputStream("C:\\Users\\user\\Desktop\\qwe1.txt");
+        ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+        outputStream.writeObject(solution);
+        outputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\user\\Desktop\\qwe1.txt");
+        ObjectInputStream inputStream = new ObjectInputStream(fileInputStream);
+        Solution solution2 = (Solution) inputStream.readObject();
+        inputStream.close();
+        solution2.close();
+    }
+}
