e9e1f924110ce9256460d21f2caea86886fcbe98
dp030592sav <7262371@gmail.com>
2018-01-25 11:10:14 +0200

    StringTokenizer javarush
    task22.task2210

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task22.task2210;
+
+import java.util.StringTokenizer;
+
+/*
+StringTokenizer
+*/
+public class Solution {
+    public static void main(String[] args) {
+        getTokens("level22.lesson13.task01", ".");
+        getTokens("level22.lesson13.task01", ".");
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        String[] res = new String[tokenizer.countTokens()];
+
+        int i = 0;
+        while (tokenizer.hasMoreElements()) {
+            res[i++] = tokenizer.nextToken();
+        }
+
+        return res;
+    }
+}
