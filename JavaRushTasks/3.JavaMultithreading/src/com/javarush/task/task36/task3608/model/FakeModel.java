697c03ec58d54a317ddc7a7797a37a09a02e636e
dp030592sav <7262371@gmail.com>
2018-02-13 14:18:42 +0200

    MVC (6) javarush

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+        users.add(new User("User1", 1, 1));
+        users.add(new User("User2", 2, 1));
+        users.add(new User("User3", 3, 2));
+        modelData.setUsers(users);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+        modelData.setDisplayDeletedUserList(false);
+    }
+
+    @Override
+    public void loadDeletedUsers() {
+        modelData.setUsers(userService.getAllDeletedUsers());
+        modelData.setDisplayDeletedUserList(true);
+    }
+}
