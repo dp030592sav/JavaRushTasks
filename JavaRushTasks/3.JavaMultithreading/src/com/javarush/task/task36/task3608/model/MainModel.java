e6e500e79b7bed923f5d8057831133107edcbbf0
dp030592sav <7262371@gmail.com>
2018-02-14 11:24:37 +0200

    MVC javarush task36.task3608

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -1,7 +1,10 @@
 package com.javarush.task.task36.task3608.model;
 
+import com.javarush.task.task36.task3608.bean.User;
 import com.javarush.task.task36.task3608.model.service.UserService;
 import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
 
 public class MainModel implements Model {
     private ModelData modelData = new ModelData();
@@ -14,7 +17,7 @@ public class MainModel implements Model {
 
     @Override
     public void loadUsers() {
-        modelData.setUsers(userService.getUsersBetweenLevels(1, 100));
+        modelData.setUsers(getAllUsers());
         modelData.setDisplayDeletedUserList(false);
     }
 
@@ -22,5 +25,25 @@ public class MainModel implements Model {
     public void loadDeletedUsers() {
         modelData.setUsers(userService.getAllDeletedUsers());
         modelData.setDisplayDeletedUserList(true);
+    }
+
+    public void loadUserById(long userId) {
+        User user = userService.getUsersById(userId);
+        modelData.setActiveUser(user);
+    }
+
+    public void deleteUserById(long userId){
+        userService.deleteUser(userId);
+        modelData.setUsers(getAllUsers());
+    }
+
+    @Override
+    public void changeUserData(String name, long id, int level) {
+        userService.createOrUpdateUser(name, id, level);
+        modelData.setUsers(getAllUsers());
+    }
+
+    private List<User> getAllUsers(){
+        return userService.filterOnlyActiveUsers(userService.getUsersBetweenLevels(1,100));
     }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -4,4 +4,7 @@ public interface Model {
     ModelData getModelData();
     void loadUsers();
     void loadDeletedUsers();
+    void loadUserById(long userId);
+    void deleteUserById(long userId);
+    void changeUserData(String name, long id, int level);
 }
