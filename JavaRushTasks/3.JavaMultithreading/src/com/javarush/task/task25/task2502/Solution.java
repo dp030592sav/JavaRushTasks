dae5632bebd640366c0c79d6299275ce909a6d85
dp030592sav <7262371@gmail.com>
2018-02-15 17:53:28 +0200

    Машину на СТО не повезем! javarush task25.task2502

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2502/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task25.task2502;
+
+import java.util.*;
+
+/* 
+Машину на СТО не повезем!
+*/
+public class Solution {
+    public static enum Wheel {
+        FRONT_LEFT,
+        FRONT_RIGHT,
+        BACK_LEFT,
+        BACK_RIGHT
+    }
+
+    public static class Car {
+        protected List<Wheel> wheels;
+
+        public Car() throws Exception {
+            wheels = new ArrayList<>();
+            String[] array = loadWheelNamesFromDB();
+            if(array.length != 4)
+                throw new Exception();
+            for (int i = 0; i < array.length; i++) {
+                wheels.add(Wheel.valueOf(array[i]));
+            }
+        }
+
+        protected String[] loadWheelNamesFromDB() {
+            //this method returns mock data
+            return new String[]{"FRONT_LEFT", "FRONT_RIGHT", "BACK_LEFT", "BACK_RIGHT"};
+        }
+    }
+
+    public static void main(String[] args) {}
+}
