bc481f02ccdce37a485cbfb14cd2571cae476fab
dp030592sav <7262371@gmail.com>
2018-03-02 11:59:40 +0200

    Чат (6) javarush task30.task3008

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -1,4 +1,21 @@
 package com.javarush.task.task30.task3008;
 
 public enum MessageType {
+    // запрос имени.
+    NAME_REQUEST,
+
+    // имя пользователя.
+    USER_NAME,
+
+    // имя принято.
+    NAME_ACCEPTED,
+
+    // текстовое сообщение.
+    TEXT,
+
+    // пользователь добавлен.
+    USER_ADDED,
+
+    // пользователь удален.
+    USER_REMOVED
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,34 @@
 package com.javarush.task.task30.task3008;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) throws IOException {
+        ConsoleHelper consoleHelper = new ConsoleHelper();
+        int port = consoleHelper.readInt();
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Cервер запущен.");
+
+        try {
+            while (true){
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (IOException e) {
+            serverSocket.close();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static class Handler extends Thread {
+        private final Socket socket;
+
+        private Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
