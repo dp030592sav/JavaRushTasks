ac3e2226f6dbe3017ae4b73442bba8438f7180d5
dp030592sav <7262371@gmail.com>
2018-03-03 11:37:36 +0200

    Чат (8) javarush task30.task3008

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,6 +3,9 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class Server {
     public static void main(String[] args) throws IOException {
@@ -12,15 +15,26 @@ public class Server {
         System.out.println("Cервер запущен.");
 
         try {
-            while (true){
+            while (true) {
                 Socket socket = serverSocket.accept();
                 new Handler(socket).start();
             }
         } catch (IOException e) {
             serverSocket.close();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println(e.getMessage());
+        }
+    }
+
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
+
+    public static void sendBroadcastMessage(Message message) {
+        for (ConcurrentMap.Entry<String, Connection> i : connectionMap.entrySet()) {
+            try {
+                i.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println(String.format("Сообщение не удалось отправить, пользователю с именем %s", i.getKey()));
+            }
         }
     }
 
@@ -29,6 +43,22 @@ public class Server {
 
         private Handler(Socket socket) {
             this.socket = socket;
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            String userName = "";
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME) continue;
+                userName = message.getData();
+                if (userName.equals("") || connectionMap.containsKey(userName)) continue;
+                connectionMap.put(userName, connection);
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                break;
+            }
+
+            return userName;
         }
     }
 }
