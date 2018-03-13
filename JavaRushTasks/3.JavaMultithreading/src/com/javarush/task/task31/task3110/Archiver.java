2bc19d00498e7f53ed90cb1854599375491d7822
dp030592sav <7262371@gmail.com>
2018-03-13 16:52:53 +0200

    Archiver (8) javarush

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,19 +1,47 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 public class Archiver {
-    public static void main(String[] args) throws Exception {
 
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-            System.out.println("Введите полный путь до архива:");
-            String pathTo = reader.readLine();
-            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathTo));
-            System.out.println("Введите полный путь до файла:");
-            String pathFrom = reader.readLine();
-            zipFileManager.createZip(Paths.get(pathFrom));
-        }
+    private static Operation operation;
+
+    public static void main(String[] args) throws Exception {
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
+        } while (operation != Operation.EXIT );
+
+//        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+//            System.out.println("Введите полный путь до архива:");
+//            String pathTo = reader.readLine();
+//            ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathTo));
+//            System.out.println("Введите полный путь до файла:");
+//            String pathFrom = reader.readLine();
+//            zipFileManager.createZip(Paths.get(pathFrom));
+//            new ExitCommand().execute();
+//        }
+    }
+
+    public static Operation askOperation() throws IOException {
+        String outForUsser = "Выберите операцию: \n";
+
+        for (Operation i : Operation.values())
+            outForUsser += String.format("%s - %s\n", i.ordinal(), i.comment);
+
+        ConsoleHelper.writeMessage(outForUsser);
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task31.task3110;
+
+public enum Operation {
+    CREATE("упаковать файлы в архив"),
+    ADD("добавить файл в архив"),
+    REMOVE("удалить файл из архива"),
+    EXTRACT("распаковать архив"),
+    CONTENT("просмотреть содержимое архива"),
+    EXIT("выход");
+
+    String comment;
+
+    Operation(String comment) {
+        this.comment = comment;
+    }
+}
