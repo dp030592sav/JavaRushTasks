a95bac5070860ba1ad70033d887f2b33d8cfb2ea
dp030592sav <7262371@gmail.com>
2018-03-15 13:20:56 +0200

    Archiver (14) javarush task31.task3110

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,8 +1,21 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
 
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage(String.format("Содержимое архива: \n%s", zipFileManager.getFilesList().toString()));
     }
 }
