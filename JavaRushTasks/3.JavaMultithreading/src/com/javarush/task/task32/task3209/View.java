c4744879e34102aa046214a4dc875033730de31d
dp030592sav <7262371@gmail.com>
2018-04-11 11:59:08 +0300

    HTML Editor (11) javarush task32.task3209

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -2,9 +2,10 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
-import javax.swing.event.ChangeListener;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,9 +18,26 @@ public class View extends JFrame implements ActionListener {
     private JTextPane htmlTextPane = new JTextPane();
     // компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое).
     private JEditorPane plainTextPane = new JEditorPane();
+    // объект для возмажости отмени действий.
+    private UndoManager undoManager = new UndoManager();
+    // объект для отследования правок, которые можно отменить или вернуть.
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException
+                | IllegalAccessException | UnsupportedLookAndFeelException e) {
+            ExceptionHandler.log(e);
+        }
+    }
 
     public Controller getController() {
         return controller;
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
     }
 
     public void setController(Controller controller) {
@@ -38,14 +56,24 @@ public class View extends JFrame implements ActionListener {
         setVisible(true);
     }
 
+    // инициализация панели меню.
     public void initMenuBar() {
+        JMenuBar jMenuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, jMenuBar);
+        MenuHelper.initEditMenu(this, jMenuBar);
+        MenuHelper.initStyleMenu(this, jMenuBar);
+        MenuHelper.initAlignMenu(this, jMenuBar);
+        MenuHelper.initColorMenu(this, jMenuBar);
+        MenuHelper.initFontMenu(this, jMenuBar);
+        MenuHelper.initHelpMenu(this, jMenuBar);
+        getContentPane().add(jMenuBar, BorderLayout.NORTH);
     }
 
     // инициализация панели редактора.
     public void initEditor() {
         htmlTextPane.setContentType("text/html");
         tabbedPane.addTab("HTML", new JScrollPane(htmlTextPane));
-        tabbedPane.addTab("Текст",  new JScrollPane(plainTextPane));
+        tabbedPane.addTab("Текст", new JScrollPane(plainTextPane));
         tabbedPane.setPreferredSize(new Dimension(500, 500));
         tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
         getContentPane().add(tabbedPane, BorderLayout.CENTER);
@@ -62,5 +90,36 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void selectedTabChanged() {
+    }
+
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
+    }
+
+    // отменяет последнее действие.
+    public void undo(){
+        try{
+            undoManager.undo();
+        }catch (Exception e){
+            ExceptionHandler.log(e);
+        }
+    }
+
+    // возвращает ранее отмененное действие.
+    public void redo(){
+        try{
+            undoManager.redo();
+        }catch (Exception e){
+            ExceptionHandler.log(e);
+        }
+
+    }
+
+    public void resetUndo(){
+        undoManager.discardAllEdits();
     }
 }
