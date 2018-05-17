3b250b1a4a5b2eea65d6935bfc802ccdc59710ac
dp030592sav <7262371@gmail.com>
2018-05-17 11:07:17 +0300

    Ресторан(21) javarush task27.task2712

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/OrderManager.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2712/OrderManager.java
@@ -1,9 +1,12 @@
 package com.javarush.task.task27.task2712;
 
+import com.javarush.task.task27.task2712.kitchen.Cook;
 import com.javarush.task.task27.task2712.kitchen.Order;
+import com.javarush.task.task27.task2712.statistic.StatisticManager;
 
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 
 // Observer(слушатель) для планшетов
@@ -14,7 +17,24 @@ public class OrderManager implements Observer {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-
+                Set<Cook> cooks = StatisticManager.getInstance().getCooks();
+                while (true) {
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                    }
+                    if (!orderQueue.isEmpty()) {
+                        for (Cook cook : cooks) {
+                            if (!cook.isBusy()) {
+                                Order order = orderQueue.poll();
+                                if (order != null)
+                                    cook.startCookingOrder(order);
+                            }
+                            if (orderQueue.isEmpty())
+                                break;
+                        }
+                    }
+                }
             }
         });
         thread.setDaemon(true);
