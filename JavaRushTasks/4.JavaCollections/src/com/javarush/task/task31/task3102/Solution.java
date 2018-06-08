30fa3052795411debd8cc66632050359699c5370
dp030592sav <7262371@gmail.com>
2018-06-08 11:31:22 +0300

    Находим все файлы javarush task31.task3102

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3102/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task31.task3102;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/* 
+Находим все файлы
+*/
+public class Solution {
+    public static List<String> getFileTree(String root) throws IOException {
+        List<String> result = new ArrayList<>();
+
+        Stack<File> stack = new Stack<>();
+        stack.push(new File(root));
+
+        while(!stack.isEmpty()) {
+            File child = stack.pop();
+            if (child.isDirectory()) {
+                for(File f : child.listFiles()) stack.push(f);
+            } else if (child.isFile()) {
+                result.add(child.getAbsolutePath());
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        getFileTree("C:\\Users\\Sloboda Alex\\Desktop\\Test");
+    }
+}
