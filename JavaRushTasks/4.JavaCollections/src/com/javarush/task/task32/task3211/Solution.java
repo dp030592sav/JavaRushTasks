e4777e98e166b2e7fdb577f1885a08bf9ed59cc1
dp030592sav <7262371@gmail.com>
2018-07-16 10:26:00 +0300

    Целостность информации javarush task32.task3211

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task32/task3211/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task32.task3211;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.DigestException;
+import java.security.MessageDigest;
+
+/* 
+Целостность информации
+*/
+
+public class Solution {
+    public static void main(String... args) throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(new String("test string"));
+        oos.flush();
+        System.out.println(compareMD5(bos, "5a47d12a2e3f9fecf2d9ba1fd98152eb")); //true
+
+    }
+
+    public static boolean compareMD5(ByteArrayOutputStream byteArrayOutputStream, String md5) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("MD5");
+        byte[] hash = digest.digest(byteArrayOutputStream.toByteArray());
+
+        StringBuffer actualMd5 = new StringBuffer();
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1) actualMd5.append('0');
+            actualMd5.append(hex);
+        }
+
+        return actualMd5.toString().equals(md5);
+    }
+}
