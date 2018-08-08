9177e3f9f6e2b5398cedf7a017173bde52065cce
dp030592sav <7262371@gmail.com>
2018-08-08 12:51:45 +0300

    Кэширование javarush task34.task3408

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3407/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3407/Solution.java
@@ -75,7 +75,6 @@ public class Solution {
         }
 
         public List<PhantomReference<Monkey>> getFilledList() {
-            ReferenceQueue<Integer> queue = new ReferenceQueue<Integer>();
             List<PhantomReference<Monkey>> list = new ArrayList<>();
 
             for (int i = 0; i < 200; i++)
