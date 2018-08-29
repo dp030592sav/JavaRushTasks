3796e5ff62ac57879e1fc738e53cb3bdd78f006f
dp030592sav <7262371@gmail.com>
2018-08-29 12:02:39 +0300

    MVC - простая версия javarush task36.task3601

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -3,6 +3,9 @@ package com.javarush.task.task37.task3702;
 import com.javarush.task.task37.task3702.female.FemaleFactory;
 import com.javarush.task.task37.task3702.male.MaleFactory;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
 public class FactoryProducer {
     public static enum HumanFactoryType{
         MALE, FEMALE;
