0126286060aa4fda37fbd9f114ff7996070a42a2
dp030592sav <7262371@gmail.com>
2018-09-17 11:37:46 +0300

    Найти класс по описанию javarush task36.task3613

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task36/task3607/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task36.task3607;
+
+
+import java.util.Collections;
+
+/*
+Найти класс по описанию
+*/
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getExpectedClass());
+    }
+
+    public static Class getExpectedClass() {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        return null;
+    }
+}
