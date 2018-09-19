310457f5393ac1c8fbfbad580ed13342b0543834
dp030592sav <7262371@gmail.com>
2018-09-19 10:53:27 +0300

    Осваиваем ClassLoader и Reflection javarush task36.task3606

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task36/task3606/Solution.java
@@ -1,8 +1,12 @@
 package com.javarush.task.task36.task3606;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +23,7 @@ public class Solution {
 
     public static void main(String[] args) throws ClassNotFoundException {
         Solution solution = new Solution(Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "com/javarush/task/task36/task3606/data/second");
+
         solution.scanFileSystem();
         System.out.println(solution.getHiddenClassObjectByKey("hiddenclassimplse"));
         System.out.println(solution.getHiddenClassObjectByKey("hiddenclassimplf"));
@@ -26,10 +31,47 @@ public class Solution {
     }
 
     public void scanFileSystem() throws ClassNotFoundException {
+        File[] files = new File(packageName).listFiles();
+
+        for (File file : files) {
+            if (file.isDirectory() || !file.getName().endsWith(".class"))
+                continue;
+
+            try {
+                hiddenClasses.add(new ModuleLoader().load(file.getPath()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public HiddenClass getHiddenClassObjectByKey(String key) {
+        for (Class c : hiddenClasses) {
+            if (c.getSimpleName().toLowerCase().startsWith(key.toLowerCase())) {
+                try {
+                    Constructor[] constructors = c.getDeclaredConstructors();
+                    for (Constructor constructor : constructors) {
+                        if (constructor.getParameterTypes().length == 0) {
+                            constructor.setAccessible(true);
+                            return (HiddenClass) constructor.newInstance(null);
+                        }
+                    }
+                } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
         return null;
+    }
+
+    public class ModuleLoader extends ClassLoader {
+
+        public Class load(String path) throws IOException {
+            byte[] bytes = Files.readAllBytes(Paths.get(String.valueOf(path)));
+
+            return defineClass(null, bytes, 0, bytes.length);
+        }
     }
 }
 
