994db6a2ebad39a60fd462d1c36240a36a45cb97
dp030592sav <7262371@gmail.com>
2018-11-02 13:54:43 +0200

    Уникальные подстроки javarush task39.task3901

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,10 +1,10 @@
 package com.javarush.task.task28.task2810;
 
-import com.javarush.task.task28.task2810.model.HHStrategy;
-import com.javarush.task.task28.task2810.model.Model;
-import com.javarush.task.task28.task2810.model.MoikrugStrategy;
-import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.view.HtmlView;
+//import com.javarush.task.task28.task2810.model.HHStrategy;
+//import com.javarush.task.task28.task2810.model.Model;
+//import com.javarush.task.task28.task2810.model.MoikrugStrategy;
+//import com.javarush.task.task28.task2810.model.Provider;
+//import com.javarush.task.task28.task2810.view.HtmlView;
 
 public class Aggregator {
     public static void main(String[] args) {
@@ -15,13 +15,13 @@ public class Aggregator {
 //
 //        view.userCitySelectEmulationMethod();
 
-        HtmlView view = new HtmlView();
-        Provider hhProvider = new Provider(new HHStrategy());
-        Provider moikrugProvider = new Provider(new MoikrugStrategy());
-        Model model = new Model(view, hhProvider, moikrugProvider);
-        //Model model = new Model(view, hhProvider);
-        Controller controller = new Controller(model);
-        view.setController(controller);
-        view.userCitySelectEmulationMethod ();
+//        HtmlView view = new HtmlView();
+//        Provider hhProvider = new Provider(new HHStrategy());
+//        Provider moikrugProvider = new Provider(new MoikrugStrategy());
+//        Model model = new Model(view, hhProvider, moikrugProvider);
+//        //Model model = new Model(view, hhProvider);
+//        Controller controller = new Controller(model);
+//        view.setController(controller);
+//        view.userCitySelectEmulationMethod ();
     }
 }
