5e6552fb5cc55a5887ab71efff617c6df1172099
dp030592sav <7262371@gmail.com>
2018-11-23 12:41:17 +0200

    Sokoban (3) javarush task34.task3410

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task34.task3410.model;
+
+//  Этот класс будет отвечать за модель нашей игры.
+public class Model {
+    // это размер ячейки игрового поля
+    public final static int FIELD_CELL_SIZE = 20;
+}
