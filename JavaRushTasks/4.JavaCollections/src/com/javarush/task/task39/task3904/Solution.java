33d5d20c9b335ccc060e1334c5d2b71369b892e6
dp030592sav <7262371@gmail.com>
2018-12-03 12:04:41 +0200

    Лестница javarush task39.task3904

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task39/task3904/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task39/task3904/Solution.java
@@ -1,31 +1,36 @@
 package com.javarush.task.task39.task3904;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
-/* 
+/*
 Лестница
 */
 public class Solution {
-    private static int n = 70;
+    private static int n = 7;
+    static HashMap<Integer, Long> map = new HashMap<>();
+    static {
+        map.put(0, 1L);
+        map.put(1, 1L);
+        map.put(2, 2L);
+        map.put(3, 4L);
+    }
+
     public static void main(String[] args) {
         System.out.println("Number of possible runups for " + n + " stairs is: " + countPossibleRunups(n));
     }
 
     public static long countPossibleRunups(int n) {
-        int result = 0;
-
-        try {
-            System.out.println();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-        }
         if(n < 0)
             return 0;
-        if(n == 1)
-            return 1;
 
-        return result;
+        if(n > 3)
+            for (int i = 4; i <= n; i++) {
+                map.put(i, map.get(i -1)
+                        +  map.get(i -2)
+                        +  map.get(i -3));
+            }
+
+        return map.get(n);
     }
 }
 
